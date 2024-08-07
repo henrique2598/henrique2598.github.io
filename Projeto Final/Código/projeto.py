328b79cd7722bd2f32a0e1047232956627603669
Henrique Vicente <henriquevicente25@hotmail.com>
2024-08-07 19:42:01 -0300

    Emotion function organization

--- Projeto Final/Código/projeto.py
+++ Projeto Final/Código/projeto.py
@@ -1,60 +1,78 @@
+#-----------------------------
+#-        IMPORTAÇÕES        -
+#-----------------------------
 import numpy as np
 import cv2
 from keras.preprocessing import image
-import keras.utils as image
+from keras.utils import img_to_array
 
 
 #-----------------------------
-#opencv initialization
-
+#-   opencv initialization   -
+#-----------------------------
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 cap = cv2.VideoCapture(0)
 
-#-----------------------------
-#face expression recognizer initialization
+
+#-----------------------------------------------
+#-  face expression recognizer initialization  -
+#-----------------------------------------------
 from keras.models import model_from_json
 model = model_from_json(open("facial_expression_model_structure.json", "r").read())
 model.load_weights('facial_expression_model_weights.h5') #load weights
-
-#-----------------------------
-
 emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
-while(True):
-	ret, img = cap.read()
-	#img = cv2.imread('C:/Users/IS96273/Desktop/hababam.jpg')
 
+
+def DetectaAluno(detected_face):
+	pass
+
+
+
+def DetectaEmocao(detected_face):
+	#detecta a emoção
+	img_pixels = img_to_array(detected_face)
+	img_pixels = np.expand_dims(img_pixels, axis = 0)
+
+	img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+	
+	predictions = model.predict(img_pixels) #store probabilities of 7 expressions
+	
+	#find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
+	max_index = np.argmax(predictions[0])
+	
+	emotion = emotions[max_index]
+
+	return emotion
+
+
+
+#----------------------------
+#-      Loop Principal      -
+#----------------------------
+while(True):
+	# Lê a webcam
+	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+	# Detecta os rostos
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-	#print(faces) #locations of detected faces
-
+	
+	# Percorre os rostos detectados
 	for (x,y,w,h) in faces:
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
 		
+		# Recorta o rosto detectado e pré-processa a imagem
 		detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
 		detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
 		detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
 		
-		img_pixels = image.img_to_array(detected_face)
-		img_pixels = np.expand_dims(img_pixels, axis = 0)
-		
-		img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
-		
-		predictions = model.predict(img_pixels) #store probabilities of 7 expressions
-		
-		#find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
-		max_index = np.argmax(predictions[0])
-		
-		emotion = emotions[max_index]
+		# Função que detecta a emoção
+		emotion = DetectaEmocao(detected_face)
 		
 		#write emotion text above rectangle
 		cv2.putText(img, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 		
-		#process on detected face end
-		#-------------------------
 
 	cv2.imshow('img',img)
 
