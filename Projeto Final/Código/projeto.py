5d26a12df784b5add082feccf5e04b2965d689f6
Henrique Vicente <henriquevicente25@hotmail.com>
2024-08-07 22:46:34 -0300

    Add Background and sequence fix

--- Projeto Final/Código/projeto.py
+++ Projeto Final/Código/projeto.py
@@ -3,7 +3,6 @@
 #-----------------------------
 import numpy as np
 import cv2
-from keras.preprocessing import image
 from keras.utils import img_to_array
 
 
@@ -12,6 +11,23 @@ from keras.utils import img_to_array
 #-----------------------------
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
+
+
+#--------------------------------
+#-   interface initialization   -
+#--------------------------------
+#Carrega o backgroud de cada etapa
+TelaInicial = cv2.imread("TelaInicial.png")
+TelaCadastro = cv2.imread("TelaCadastro.png")
+TelaEncerramento = cv2.imread("TelaEncerramento.png")
+
+# Define a janela de exibição das imagens, com tamanho automático
+winName = 'Chamada Virtual'
+cv2.namedWindow(winName, cv2.WINDOW_FULLSCREEN)
+
+# Posiciona a janela na metade direita do (meu) monitor
+cv2.moveWindow(winName, 100, 100)
+
 
 
 #-----------------------------------------------
@@ -61,23 +77,16 @@ def GerarRelatorio():
 #-      Loop Principal      -
 #----------------------------
 
-# Define o tamanho desejado para a janela
-w = 960
-h = 1080
-# Define a janela de exibição das imagens, com tamanho automático
-winName = 'Chamada Virtual'
-canvas = np.ones((300, 400, 3)) * 255 #imagem 400x300, com fundo branco e 3 canais para as cores
-cv2.namedWindow(winName, cv2.WINDOW_FULLSCREEN)
-# Posiciona a janela na metade direita do (meu) monitor
-cv2.moveWindow(winName, 100, 100)
 
 # Repetição - Dia de Aula
+Periodo = 0
 DandoAula=True
+Cadastrando=False
+
+
 while(DandoAula==True):
 	#Exibe tela inicial com informações da turma
-	Periodo = 0
-
-	cv2.imshow(winName, canvas)
+	cv2.imshow(winName, TelaInicial)
 
 	# Decide se é entrada ou saída dos alunos
 	while (Periodo==0):
@@ -86,22 +95,24 @@ while(DandoAula==True):
 		elif cv2.waitKey(1) & 0xFF == ord('s'):
 			Periodo = 2
 
+	# Habilita a etapa de cadastro
+	Cadastrando = True
+
 	# Exibe o Loop para registro dos alunos
-	Cadastrando=True
 	while(Cadastrando==True):
 		# Lê a webcam
-		ret, img = cap.read()
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		ret, cam = cap.read()
+		gray = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
 
 		# Detecta os rostos
 		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 		
 		# Percorre os rostos detectados
 		for (x,y,w,h) in faces:
-			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+			cv2.rectangle(cam,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
 			
 			# Recorta o rosto detectado e pré-processa a imagem
-			detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+			detected_face = cam[int(y):int(y+h), int(x):int(x+w)] #crop detected face
 			detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
 			detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
 			
@@ -111,14 +122,15 @@ while(DandoAula==True):
 			emotion = DetectaEmocao(detected_face)
 			
 			#write emotion text above rectangle
-			cv2.putText(img, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+			cv2.putText(cam, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 			
 		# Atualiza a tela de cadastro
+		img = cv2.hconcat([cam, TelaCadastro])
 		cv2.imshow(winName,img)
 
 		# Verifica se o cadastro foi concluído
 		if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
-			Cadastrando=False
+			Cadastrando = False
 	
 	# Decide o fluxo com base no período
 	if Periodo == 1:
@@ -128,8 +140,8 @@ while(DandoAula==True):
 		# Finalizou a saída - gerar relatório e encerrar o programa
 		GerarRelatorio()
 		# Atualiza a tela de encerramento
-		cv2.imshow(winName,img)
-		DandoAula==False
+		cv2.imshow(winName,TelaEncerramento)
+		DandoAula = False
 
 
 #kill open cv things		
